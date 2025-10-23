7790e497880f9974a5292243d00f8b855d751c5b
muuhesham <muhammedhesham488@gmail.com>
2025-10-23 19:43:53 +0300

    Clear main branch: remove all files

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const config = require('./Config/env');
-const app = require('./app');
-const {connectDB} = require('./Config/db');
-
-connectDB();
-
-app.listen(config.PORT, config.HOSTNAME, () => {
-    console.log(`APP RUNNING on http://${config.HOSTNAME}:${config.PORT}`);
-})
