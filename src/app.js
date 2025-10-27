8f352600b5b610902603898ee00dc3109ebf3db2
lime0x001 <mohammada4raf@gmail.com>
2025-10-27 18:54:05 +0300

    feat: update routing structure to include auth routes in app.js

--- src/app.js
+++ src/app.js
@@ -3,17 +3,18 @@ const app = express();
 
 //! BEFORE MIDDLEWARES
 import { cors, corsOptions } from './config/cors.js';
-import { activityLogger } from "./middlewares/activityLogger.js";
+import { activityLogger } from './middlewares/activityLogger.js';
+
+
+//! ROUTES
+import authRoutes from './routes/auth.routes.js';
 
 app.use(cors(corsOptions));
 app.use(activityLogger);
 app.use(express.json());
 
 
-//! ROUTES
-import v1Routes from './routes/v1/index.js';
-
-app.use('/api/v1', v1Routes);
+app.use('/api/v1/auth', authRoutes);
 
 
 export default app;
