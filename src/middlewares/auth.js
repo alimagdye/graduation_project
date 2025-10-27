e96d4b4a6054eba84d6ce8ef03bee4c2e8dddabe
lime0x001 <mohammada4raf@gmail.com>
2025-10-27 18:50:37 +0300

    feat: update auth middleware to use JWT_KEY instead of APP_SECRET_KEY

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,20 @@
 import jwt from 'jsonwebtoken';
 import { sendError } from '../utils/response.js';
-import { APP_SECRET_KEY } from './../config/env.js';
+import { JWT_KEY } from './../config/env.js';
 
 function auth(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     
     if (!token) {
-        return sendError(res, 'No auth token provided', null, 401);
+        return sendError(res, 'No auth token provided', null, null,401);
     }
 
     try {
-        req.user = jwt.verify(token, APP_SECRET_KEY);
+        req.user = jwt.verify(token, JWT_KEY);
         next(); 
     } catch (error) {
-        return sendError(res,'Invalid auth token.', null, 403);
+        return sendError(res,'Invalid auth token.', null, null,403);
     }
 }
 
