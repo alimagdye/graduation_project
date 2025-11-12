2aa3b66a9d13fe1100c6e250c185dbb92aa02e57
lime0x001 <mohammada4raf@gmail.com>
2025-11-13 01:10:27 +0200

    feat: enhance auth middleware to check token validity with authService

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,22 @@
 import jwt from 'jsonwebtoken';
 import { sendError } from '../utils/response.js';
 import { JWT_KEY } from './../config/env.js';
-
-function auth(req, res, next) {
+import authService from '../services/authService.js';
+async function auth(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     
     if (!token) {
         return sendError(res, 'No auth token provided', null, null,401);
     }
+    
+    const isAlive = await authService.isAccessAlive({ accessToken: token });
+    if (!isAlive) {
+        return sendError(res,  'Token revoked', 'REVOKED_TOKEN', null, 401)
+    }
 
     try {
+        req.accessToken = token;
         req.user = jwt.verify(token, JWT_KEY);
         next(); 
     } catch (error) {
