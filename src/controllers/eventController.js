c3a67995d1993e6a07d6b73edb93527223f2783e
lime0x001 <mohammada4raf@gmail.com>
2025-11-20 15:56:40 +0200

    feat: add eventController for handling event-related requests

---
+++ src/controllers/eventController.js
@@ -0,0 +1,8 @@
+import { sendSuccess, sendFail, sendError } from '../utils/response.js';
+import CommonErrors from '../constants/errors/common.js';
+import eventService from '../services/eventService.js';
+
+const eventController = {};
+
+
+export default eventController;
