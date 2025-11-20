e6d753ed2b14ba611f0d4045f206d4f655ae1e71
lime0x001 <mohammada4raf@gmail.com>
2025-11-20 15:59:12 +0200

    feat: add organizer routes for event management

---
+++ src/routes/organizer.routes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import organizerController from '../controllers/organizerController.js';
+import organizerValidation from '../validations/organizerValidation.js';
+import validate from '../middlewares/validate.js';
+import auth from '../middlewares/auth.js';
+import authorize from '../middlewares/authorize.js';
+import { publicLimiter } from '../middlewares/rateLimiter.js';
+import assertMultipart  from '../middlewares/assertMultipart.js';
+import { upload } from '../middlewares/upload.js';
+import parseJsonFields from '../middlewares/parseJson.js';
+
+const Router = express.Router();
+
+Router.post(
+    '/events',
+    publicLimiter,
+    auth,
+    assertMultipart,
+    authorize.isOrganizer,
+    upload.single('banner'),
+    parseJsonFields(['location', 'tickets']),
+    organizerValidation.createEvent,
+    validate,
+    organizerController.createEvent
+);
+
+//Router.put('/events/:eventId', publicLimiter, authorize.isOrganizer, eventValidation.create, validate, organizerController.updateEvent);
+//Router.delete('/events/:eventId', publicLimiter, authorize.isOrganizer, organizerController.deleteEvent);
+//Router.get('/events', publicLimiter, authorize.isOrganizer, organizerController.listEvents);
+//Router.get('/events/:eventId/registrations', publicLimiter, authorize.isOrganizer, organizerController.listRegistrations);
+//Router.put('/registrations/:registrationId', publicLimiter, authorize.isOrganizer, organizerController.manageRegistration);
+
+export default Router;
