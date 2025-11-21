a7e450c316b85e8b8cdd9f3560687633cd55c3bf
muuhesham <muhammedhesham488@gmail.com>
2025-11-21 23:45:39 +0200

    fix: add the quantity in venue service layer to avoid errors with database

--- src/services/venueService.js
+++ src/services/venueService.js
@@ -1,22 +1,21 @@
 import { prisma as prismaClient } from '../config/db.js';
 
 const venueService = {
-    async create(eventId, { name, address, capacity, city, country, zipCode, longitude, latitude }, tx = prismaClient) {
+    async create(eventId, { name, address, city, country, zipCode, longitude, latitude, googlePlaceId }, tx = prismaClient) {
         return tx.venue.create({
             data: {
                 eventId,
                 name,
                 address,
-                capacity,
                 city,
                 country,
                 zipCode,
                 longitude,
                 latitude,
+                googlePlaceId,
             },
         });
     },
-
 
     async getVenues() {
         return prismaClient.venue.findMany();
