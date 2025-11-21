cc4d727f5e632a8db1d8dead65e5d51fb8d56fac
muuhesham <muhammedhesham488@gmail.com>
2025-11-21 23:49:07 +0200

    feat: add functions in ticketType service layer to handle the paid & free tickets plus get total tickets by event

--- src/services/ticketTypeService.js
+++ src/services/ticketTypeService.js
@@ -1,6 +1,7 @@
 import { prisma as prismaClient } from './../config/db.js';
 
 const ticketTypeService = {
+    //CREATE BULK TICKETS
     async createBulk(eventId, ticketTypes, tx = prismaClient) {
         const ticketTypeData = ticketTypes.map(ticket => ({
             eventId,
@@ -8,12 +9,33 @@ const ticketTypeService = {
             price: ticket.price,
             quantity: ticket.quantity,
         }));
-        
         return tx.ticketType.createMany({
             data: ticketTypeData,
         });
     },
     
+    //CREATE FREE BULK TICKET
+    async createFreeBulk(eventId, ticketTypes, tx= prismaClient){
+        const ticketTypeData = ticketTypes.map(ticket => ({
+            eventId,
+            name: ticket.name || 'Free Ticket',
+            price: 0,
+            quantity: ticket.quantity,
+        }));
+        return tx.ticketType.createMany({
+            data: ticketTypeData
+        })
+    },
+
+    //GET TOTAL TICKETS FOR EVENT
+    async getTotalTickets(eventId){
+        const totalTickets = await prismaClient.ticketType.aggregate({
+            where: {eventId},
+            _sum: {quantity: true},
+        });
+        return totalTickets._sum.quantity || 0;
+    },
+
     async getAllTicketTypes(eventId) {
         return prismaClient.ticketType.findMany({
             where: { eventId },
