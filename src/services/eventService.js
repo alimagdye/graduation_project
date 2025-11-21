33b644125099eea7207f2f22f2ca73f1546fdc83
muuhesham <muhammedhesham488@gmail.com>
2025-11-21 23:52:18 +0200

    feat: add fucntion to create session to handle the Date with new table EventSession and add slug and new data in create function in table event

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,33 +1,47 @@
 import { prisma as prismaClient } from '../config/db.js';
 import eventStatus from '../constants/enums/eventStatus.js';
 import fileService from './fileService.js';
+import slugify from 'slugify';
 
 const eventService = {
     DEFAULT_MEDIA_FOLDER: 'events',
     
-    async create(organizerId, { title, category, description, startDate, endDate, status = eventStatus.ACTIVE, banner }, tx = prismaClient) {
+    // CREATE 
+    async create(organizerId, { title, description, status = eventStatus.ACTIVE, eventMode, eventType, banner, venueId, categoryId }, tx = prismaClient) {
         const { disk: bannerDisk, url: bannerPath } = await eventService.handleBanner(banner);
+        const slug = slugify(title, {lower: true, strict: true});
         
         return tx.event.create({
             data: {
                 organizerId,
                 title,
+                slug,
                 description,
                 bannerDisk,
                 bannerPath,
-                startDate,
-                endDate,
                 status,
+                eventMode,
+                eventType,
+                venueId,
+                categoryId
             },
         });
     },
     
+    async createSession(eventId, {startDate, endDate}, tx= prismaClient){
+        return tx.eventSession.create({
+            data: {
+                eventId,
+                startDate,
+                endDate,
+            },
+        })
+    },
+
     async handleBanner(banner) {
         if (!banner) return null;
-
         return await fileService.save(banner, eventService.DEFAULT_MEDIA_FOLDER);
     }
 };
-
 
 export default eventService;
