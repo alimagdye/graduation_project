5a91f38d7e61806d9bb8660075d8d2cb037e9790
lime0x001 <mohammada4raf@gmail.com>
2025-11-23 21:14:51 +0200

    feat: enhance category service with default selections, exclude fields, and pagination support

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,10 +1,40 @@
-import { Prisma as prismaClient } from "@prisma/client";
+import { prisma as prismaClient } from '../config/db.js';
+import { PrismaQueryBuilder } from '../utils/queryBulider.js';
+import eventService from "./eventService.js";
 
 const categoryService = {
+    DEFAULT_SELECTIONS: {
+        id: true,
+        name: true,
+        description: true,
+        createdAt: true,
+        updatedAt: true,
+    },
+    
+    DEFAULT_EXCLUDE_FIELDS: {
+        updatedAt: true,
+    },
 
-    async getByCategory(categoryName, tx= prismaClient){
+    DEFAULT_RELATIONS: {
+    },
+    
+    async getByCategory(categoryName, tx = prismaClient){
         return tx.category.findUnique({
             where: {name: categoryName},
+        });
+    },
+    
+    async getAll({ selections = categoryService.DEFAULT_SELECTIONS, relations = categoryService.DEFAULT_RELATIONS, exclude = eventService.DEFAULT_EXCLUDE_FIELDS }, { limit = 10, page = 1 }) {
+        const query = new PrismaQueryBuilder()
+            .paginate(page, limit)
+            .select(selections)
+            .include(relations)
+            .omit(exclude)
+            .value;
+        
+        return prismaClient.category.findMany({
+            where: {},
+            ...query
         });
     },
 }
