82e165220607e69d4607d92cd52b6dd0cd6c17a6
lime0x001 <mohammada4raf@gmail.com>
2025-11-26 18:45:07 +0200

    feat: add new events retrieval and past events highlights to home service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,26 +1,39 @@
 import asyncWrapper from '../middlewares/asyncWrapper.js';
-import { parsePagination } from '../utils/requestHelpers.js';
+import { parsePagination } from '../utils/paginationHelpers.js';
 import { sendSuccess } from '../utils/response.js';
 import homeService from '../services/homeService.js';
 
 const homeController = {
     latestEvents: asyncWrapper(async (req, res) => {
         const { page, limit } = parsePagination(req.query);
-        
-        const result = await homeService.latestEvents({
-            selections: null, relations: { venue: true}, limit,  page
-        });
-        
+
+        const result = await homeService.latestEvents({ limit, page });
+
+        return sendSuccess(res, { events: result });
+    }),
+    
+    
+    newEventsThisWeek: asyncWrapper(async (req, res) => {
+        const { page, limit } = parsePagination(req.query);
+
+        const result = await homeService.newEventsThisWeek({ limit,  page });
+
         return sendSuccess(res, { events: result });
     }),
     
     allCategories: asyncWrapper(async (req, res) => {
         const { page, limit } = parsePagination(req.query);
 
-        const result = await homeService.getCategories(
-            { selections: null, relations: null, limit, page }
-        );
+        const result = await homeService.getCategories({ limit, page });
         return sendSuccess(res, { categories: result });
+    }),
+    
+    pastEventsAndHighlights: asyncWrapper(async (req, res) => {
+        const { page, limit } = parsePagination(req.query);
+
+        const result = await homeService.pastEventsAndHighlights({ limit, page });
+
+        return sendSuccess(res, { events: result });
     }),
 };
 
